9361e6dd9a29f677454e00fe37f70bef0880fb71
Nicolo Mariani <marianinicolo@itis-molinari.eu>
2025-05-05 19:38:29 +0200

    Inizializzazione progetto

---
+++ database.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const mysql = require('mysql2');
+const { isNativeError } = require('util/types');
+let conf = JSON.parse(fs.readFileSync('public/conf.json'));
+conf.ssl = {
+    ca: fs.readFileSync(__dirname + '/ca.pem')
+}
+const connection = mysql.createConnection(conf);
+
+
+const executeQuery = (sql) => {
+   return new Promise((resolve, reject) => {      
+         connection.query(sql, function (err, result) {
+            if (err) {
+               console.error(err);
+               reject();     
+            }   
+            console.log('done');
+            resolve(result);         
+      });
+   })
+}
+
+const database = {
+
+   insert: async (book) => {
+      //INSERIMENTO ELEMENTO NELLA TABELLA
+      let sql = `
+         INSERT INTO booking (idType, date, hour, name)
+         VALUES ('${book.idType}', '${book.date}', '${book.hour}', '${book.name}')
+           `;
+      console.log("SQL INSERIMENTO-> ", book);
+      return await executeQuery(sql)
+    },
+
+   delete: (id) => {
+      //ELIMINAZIONE ELEMENTO DALLA TABELLA
+      let sql = `
+      DELETE FROM booking
+      WHERE id = $ID
+      `;
+      sql = sql.replace('$ID', id)
+      return executeQuery(sql);
+    }, 
+
+   update: (orario) => {
+      //MODIFICARE DOPO
+      let sql = `
+      UPDATE todo
+      SET completed=$COMPLETED
+      WHERE id=$ID
+         `;
+      sql = sql.replace("%ID", todo.id);
+      sql = sql.replace("%COMPLETED", todo.completed);
+      return executeQuery(sql); 
+   },
+
+}
+
+module.exports = database;
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const http = require('http');
+const path = require('path');
+const app = express();
+//const multer  = require('multer');
+const database = require('./database.js');
+app.use(express.json());
+
+database.createTableType();
+database.createTableBooking();
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/files", express.static(path.join(__dirname, "files")));
+
+app.post("/upload", async (req, res) => {
+    
+});
+
+app.get('/get', async (req, res) => {
+
+})
+
+app.get('/gettips', async (req, res) => {
+
+})
+
+
+app.delete('/delete/:id', async (req, res) => {
+
+})
+
+const server = http.createServer(app);
+server.listen(5500, () => {
+  console.log("- server running");
+});
