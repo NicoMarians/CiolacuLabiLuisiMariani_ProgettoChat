922946c764074124b15d4ed9d8f4c25c19736964
tommaso <ciolacutommaso@itis-molinari.eu>
2025-05-11 18:28:51 +0200

    Aggiornato createMessageFun per includere idChat e modificato createMessage per gestire il nuovo parametro. Aggiunta logica per il rendering dei messaggi in base all'idChat e implementata la funzione setMess per inizializzare la lista dei messaggi. Modificato index.js per integrare il nuovo chatComp e gestire le sottoscrizioni pubsub.

--- public/View - components/chat.js
+++ public/View - components/chat.js
@@ -1,4 +1,4 @@
-const createMessageFun = (content, time_stamp, idUser) => {
+const createMessageFun = (content, time_stamp, idUser, idChat) => {
     // TIME stamp contiene data e ora
     return {
         content: "",
@@ -8,19 +8,38 @@ const createMessageFun = (content, time_stamp, idUser) => {
 }
 
 const createChatComp = (parentElementIn, pubsub) => {
-    listMess = [];
     parentElement = parentElementIn;
+    listMess = [];
+    currentUserId = 0;
     return {
-        createMessage: (content, time_stamp, idUser) => {
-            newMess = createMessageFun(content, time_stamp, idUser)
+        createMessage: (content, time_stamp, idUser, idChat) => {
+            newMess = createMessageFun(content, time_stamp, idUser, idChat)
             listMess.push(newMess);
-            pubsub.publish("render-chat", listMess); //CREARE LA FUNZIONE PUBSUB ("render-chat") DENTRO INDEX.JS
+            //pubsub.publish("render-chat", listMess); //CREARE LA FUNZIONE PUBSUB ("render-chat") DENTRO INDEX.JS
         },
 
-        render: () => { 
-            
+        render: (idChat_in) => { 
+            const template = `<a><li>%CHAT</li></a`
+            let html = "";
+            listMess.forEach(messaggio => {
+                if (messaggio.idUser == currentUserId && messaggio.idChat == idChat_in)
+                html += template.replace("%CHAT", messaggio);
+            });
+        },
+        setMess: (ListIn) => {
+            //PRENDE UNA LISTA DI DIZIONARI CON (CONTENT, TIME, IDUSER, IDCHAT)
+            listMess = ListIn;
+            ListIn.forEach((mess) => {
+                listMess.push(createMessageFun(mess.content, mess.time_stamp, mess.idUser, mess.idChat))
+            })
+        },
+        addMess: (messObj) => {
+            //FNZ CHE AGGIUNGE UN OGGETTO MESS ALLA LISTA INTERNA DEL COMPONENTE
+            //PASSARE UN DIZIONARIO CON (CONTENT, TIME, IDUSER, IDCHAT)
+            listMess.push(createMessageFun(messObj.content, messObj.time_stamp, messObj.idUser, messObj.idChat))
         }
     }
 }
 
 
+module.exports = createChatComp;
--- public/index.js
+++ public/index.js
@@ -1,9 +1,15 @@
 // -- Parsing -- 
+const a = '';
+
+
+document.body.focus();
 
 
 // -- Business Logic -- 
 import {generatePubSub} from './BL - components/pubsub.js';
 import {createMiddleware} from './BL - components/middleware.js';
+import {createChatComp} from './View - components/chat.js'
+import {createNavigator} from "./View - components/navigator.js";
 
 // -- View --
 
@@ -15,5 +21,21 @@ import {createMiddleware} from './BL - components/middleware.js';
 fetch("./conf.json").then(r => r.json()).then(conf => {
     const pubsub = generatePubSub();
     const middleware = createMiddleware();
+    const chatComp = createChatComp(a,pubsub)
+    const navigator = createNavigator(document.querySelector("#container"),detailComp);
+
+
+
+    pubsub.subscribe("render-chat", () => {
+        chatComp.render();
+    }); 
+    pubsub.subscribe("set-data", (list) =>{
+        //PASSARE UNA LISTA DI DIZIONARIO CON SEGUENTE FORMATO -> content, time_stamp, idUser, idChat
+        chatComp.addMess(list);
+        middleware.createMessage()
+    });
+
+
+
 
 })
