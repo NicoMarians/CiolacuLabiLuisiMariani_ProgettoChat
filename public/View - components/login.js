f4871ece814430aa5653489ddea481d55fb97514
Nicolo <marianinicolo@itis-molinari.eu>
2025-05-11 21:38:27 +0200

    binding e inizio login

---
+++ public/View - components/login.js
@@ -0,0 +1,6 @@
+export const createLogin = async () => {
+    
+    return {
+        
+    }
+}
--- public/index.js
+++ public/index.js
@@ -1,9 +1,17 @@
 // -- Parsing -- 
-
+const buttonLogin = document.getElementById("buttonLogin");
+const buttonRegister = document.getElementById("buttonRegister");
+const buttonCreateChat = document.getElementById("buttonCreateChat");
+const searchBar = document.getElementById("seatchBar");
+const divUsername = document.getElementById("divUsername");
+const divProfilePicture = document.getElementById("divProfilePicture");
 
 // -- Business Logic -- 
 import {generatePubSub} from './BL - components/pubsub.js';
 import {createMiddleware} from './BL - components/middleware.js';
+import {createLogin} from './View - components/login.js';
+import {createChatList} from './View - components/list.js';
+import { createNavigator } from './View - components/navigator.js';
 
 // -- View --
 
