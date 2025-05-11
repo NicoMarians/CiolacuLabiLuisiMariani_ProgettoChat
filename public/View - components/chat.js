64f4874499fe10b0aec632b757b43f9ea080531c
tommaso <ciolacutommaso@itis-molinari.eu>
2025-05-11 21:35:05 +0200

    Rimosso il parametro idChat dalla funzione render e semplificata la logica di rendering dei messaggi. Modificata la funzione setMess per accettare direttamente oggetti messaggio e aggiornato addMess per utilizzare oggetti messaggio senza chiamare createMessageFun.

--- public/View - components/chat.js
+++ public/View - components/chat.js
@@ -18,25 +18,20 @@ const createChatComp = (parentElementIn, pubsub) => {
             //pubsub.publish("render-chat", listMess); //CREARE LA FUNZIONE PUBSUB ("render-chat") DENTRO INDEX.JS
         },
 
-        render: (idChat_in) => { 
-            const template = `<a><li>%CHAT</li></a`
-            let html = "";
+        render: () => { 
+            const template = `<li>%CHAT</li>`
             listMess.forEach(messaggio => {
-                if (messaggio.idUser == currentUserId && messaggio.idChat == idChat_in)
-                html += template.replace("%CHAT", messaggio);
+                html += template.replace("%CHAT", messaggio.content);
             });
         },
         setMess: (ListIn) => {
             //PRENDE UNA LISTA DI DIZIONARI CON (CONTENT, TIME, IDUSER, IDCHAT)
             listMess = ListIn;
-            ListIn.forEach((mess) => {
-                listMess.push(createMessageFun(mess.content, mess.time_stamp, mess.idUser, mess.idChat))
-            })
         },
         addMess: (messObj) => {
             //FNZ CHE AGGIUNGE UN OGGETTO MESS ALLA LISTA INTERNA DEL COMPONENTE
             //PASSARE UN DIZIONARIO CON (CONTENT, TIME, IDUSER, IDCHAT)
-            listMess.push(createMessageFun(messObj.content, messObj.time_stamp, messObj.idUser, messObj.idChat))
+            listMess.push(messObj);
         }
     }
 }
