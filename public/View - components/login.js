40ef85385b102ebbbab9e656613ad8ddcabb2a76
Mattia Luisi <luisimattia@itis-molinari.eu>
2025-05-12 17:12:56 +0200

    metodo per controllare la login

--- public/View - components/login.js
+++ public/View - components/login.js
@@ -7,6 +7,42 @@ export const createLogin = async (username,password) => {
         //Metodo per impostare l'elemento di binding
         setBingingElement: function (element) {
             bindingElement = element;
-        }
+        },
+
+        //Metodo per confermare la login
+        confirmLogin: async function () {
+            try {
+                const response = await fetch("/login", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body : JSON.stringify({username, password}),
+                });
+                const data = await response.json():
+
+                if (data.resutl === "ok") {
+                    isLogged = true;
+                    return {
+                        succes: true,
+                        user: data.user,
+                    };
+                } else {
+                    isLogged = false;
+                    return {
+                        succes: false,
+                        message: data.message || "Credenziali errate",
+                    };
+                    
+                }
+            } catch (error) {
+                isLogged = false;
+                return {
+                    succes: false,
+                    message: "Errore di rete",
+                };
+            }
+        },
+        
     }
 }
